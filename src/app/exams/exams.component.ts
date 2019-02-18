e280e4ab6320bd3a3b414758ad2128b1358f3cec
Gabriel Fior <gabrielfior@Gabriels-MacBook-Pro.local>
2019-02-18 11:39:20 -0300

    enabling admins to delete exams

--- src/app/exams/exams.component.ts
+++ src/app/exams/exams.component.ts
@@ -10,29 +10,33 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 @Component({
   selector: 'exams',
   template: `
-  <h2>Exams</h2>
-  <p>Choose an exam and start studying.</p>
-  <div class="exams">
-    <mat-card class="example-card" *ngFor="let exam of examsList" class="mat-elevation-z5">
-      <mat-card-content>
-        <mat-card-title>{{exam.title}}</mat-card-title>
-        <mat-card-subtitle>{{exam.description}}</mat-card-subtitle>
-        <p>
-          Etiam enim purus, vehicula nec dapibus quis, egestas eu quam.
-          Nullam eleifend auctor leo, vitae rhoncus mi sodales vel.
-          Aenean fermentum laoreet volutpat. Integer quam orci,
-          molestie non nibh suscipit, faucibus euismod sapien.
-        </p>
-        <button mat-raised-button color="accent">Start Exam</button>
-      </mat-card-content>
-    </mat-card>
-  </div>
-  <button mat-fab color="primary" *ngIf="authenticated"
-          class="new-exam" routerLink="/new-exam">
-    <i class="material-icons">note_add</i>
-  </button>
-`,
-styleUrls: ['exams.component.css'],
+    <h2>Exams</h2>
+    <p>Choose an exam and start studying.</p>
+    <div class="exams">
+      <mat-card class="example-card" *ngFor="let exam of examsList" class="mat-elevation-z5">
+        <mat-card-content>
+          <mat-card-title>{{exam.title}}</mat-card-title>
+          <mat-card-subtitle>{{exam.description}}</mat-card-subtitle>
+          <p>
+            Etiam enim purus, vehicula nec dapibus quis, egestas eu quam.
+            Nullam eleifend auctor leo, vitae rhoncus mi sodales vel.
+            Aenean fermentum laoreet volutpat. Integer quam orci,
+            molestie non nibh suscipit, faucibus euismod sapien.
+          </p>
+          <button mat-raised-button color="accent">Start Exam</button>
+          <button mat-button color="warn" *ngIf="isAdmin()"
+                  (click)="delete(exam.id)">
+            Delete
+          </button>
+        </mat-card-content>
+      </mat-card>
+    </div>
+    <button mat-fab color="primary" *ngIf="authenticated"
+            class="new-exam" routerLink="/new-exam">
+      <i class="material-icons">note_add</i>
+    </button>
+  `,
+  styleUrls: ['exams.component.css'],
 })
 export class ExamsComponent implements OnInit, OnDestroy {
   examsListSubs: Subscription;
@@ -56,4 +60,27 @@ export class ExamsComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.examsListSubs.unsubscribe();
   }
+
+  delete(examId: number) {
+    this.examsApi
+      .deleteExam(examId)
+      .subscribe(() => {
+        this.examsListSubs = this.examsApi
+          .getExams()
+          .subscribe(res => {
+              this.examsList = res;
+            },
+            console.error
+          )
+      }, console.error);
+  }
+
+  isAdmin() {
+    if (!Auth0.isAuthenticated()) return false;
+
+    const roles = Auth0.getProfile()['https://online-exams.com/roles'];
+    console.log(roles);
+    return roles.includes('admin');
+  }
+
 }
