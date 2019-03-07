334b5f6f81f2014ae96fdffdefd3bc676a7f9665
Gabriel Fior <gabrielfior@Gabriels-MacBook-Pro.local>
2019-03-07 22:09:39 +0100

    added support for environments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,8 @@ import { MenteeDetailComponent } from './mentee/mentee-detail/mentee-detail.comp
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MenteePageModule } from './mentee/mentee.module';
 import { MentorPageModule } from './mentor/mentor.module';
+import { environment } from '../environments/environment';
+
 import {
   MatAutocompleteModule,
   MatBadgeModule,
@@ -64,7 +66,7 @@ import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {OWL_DATE_TIME_FORMATS } from 'ng-pick-datetime';
 import { DatePipe } from '@angular/common'
-import {AWS_URL} from './env.prod';
+
 import { MentorListingComponent } from './mentor/mentor-listing/mentor-listing.component';
 import { MentorInformationComponent } from './mentor/mentor-information/mentor-information.component';
 import { MentorDetailComponent } from './mentor/mentor-detail/mentor-detail.component';
@@ -185,7 +187,7 @@ export class AppModule {
       domain: 'brasa-pre-app.eu.auth0.com',
       audience: 'https://online-exam.digituz.com',
       clientID: 'Qw4p5JyU5yO2dZ0DWPL27g9tq6P4c5kk',
-      redirectUri: AWS_URL,
+      redirectUri: environment.HOSTNAME + '/callback',
       scope: 'openid profile manage:exams'
     });
   }
--- src/app/env.prod.ts
+++
@@ -1,1 +0,0 @@
-export const AWS_URL = 'http://ec2-52-53-185-200.us-west-1.compute.amazonaws.com:4300/callback';
